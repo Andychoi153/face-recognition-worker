0ce3e7191b3b7699e7e6c978ede50c7a92a22a24
Unknown <Andychoi153@gmail.com>
2018-11-10 16:59:54 +0900

    test request for create contract

--- test_requests.py
+++ test_requests.py
@@ -3,12 +3,35 @@ cnnOut = 2
 import requests
 import json
 
-data = {'req_addr': str(addr[0]) + ':' + str(addr[1]),
+data = {'account': 'MES'}
+
+response = requests.post('http://127.0.0.1:5000/get_account', json=data)
+print('MES')
+print(response.text)
+
+data = {'account': 'REQUESTER1'}
+response = requests.post('http://127.0.0.1:5000/get_account', json=data)
+print('REQUESTER1')
+print(response.text)
+
+data = {'req_addr': 'REQUESTER1',
         'data': {'hash': hash(str(cnnOut)),
                  'sol': {'name': 'hello',
-                         'age': 'goodmorning'}
+                         'age': 1}
                  },
         'time_stamp': '201821199'
         }
-requests.post('http://127.0.0.1:5000/create_transaction_by_contract', json=data)
-print(json.dumps(data))
+response = requests.post('http://127.0.0.1:5000/create_transaction_by_contract', json=data)
+print(response.text)
+
+data = {'account': 'MES'}
+
+response = requests.post('http://127.0.0.1:5000/get_account', json=data)
+print('MES')
+print(response.text)
+
+data = {'account': 'REQUESTER1'}
+
+response = requests.post('http://127.0.0.1:5000/get_account', json=data)
+print('REQUESTER1')
+print(response.text)
